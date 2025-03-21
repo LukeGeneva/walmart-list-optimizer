231dd465799123db5565e853f43f07cd3611ac5f
Luke Geneva <lukegeneva@gmail.com>
2025-03-20 21:49:45 -0500

    Change project to Chrome extension

---
+++ content.ts
@@ -0,0 +1,24 @@
+const observer = new MutationObserver(findLocation);
+observer.observe(document.body, { childList: true, subtree: true });
+
+function findLocation() {
+  const location = document.querySelector<HTMLSpanElement>('div[data-testid="product-aisle-location"] span')
+  console.log('LOCATION ELEMENT', location)
+  if (location?.innerText) injectAddToListButton()
+}
+
+function injectAddToListButton() {
+  if (document.getElementById('add-to-list')) return;
+
+  const addToCartButton = document.querySelector<HTMLButtonElement>('button[data-automation-id="atc"]')
+  if (!addToCartButton) return;
+
+  const addToListButton = document.createElement('button');
+  addToListButton.id = 'add-to-list'
+  addToListButton.innerHTML = 'Add to List'
+  addToListButton.onclick = () => {
+    alert('Added to List!');
+    addToCartButton.click();
+  }
+  addToCartButton.parentNode?.appendChild(addToListButton);
+}
